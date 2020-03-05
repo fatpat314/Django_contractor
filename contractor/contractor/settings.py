0581042c6106711d0d2e20043ec100ecd12fb1d1
Patrick <w.patrick.kelly@gmail.com>
2020-03-05 14:16:24 -0800

    deployment

--- contractor/contractor/settings.py
+++ contractor/contractor/settings.py
@@ -135,9 +135,9 @@ DEFAULT_LOGOUT_URL = "/"
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS =[
-#     os.path.join(BASE_DIR, "static")
-# ]
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, "static")
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
