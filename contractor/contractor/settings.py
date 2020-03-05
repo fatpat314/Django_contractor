2383c477ce5beafe3956508a7346d5d49fa2b19a
Patrick <w.patrick.kelly@gmail.com>
2020-03-05 14:17:33 -0800

    deployment

--- contractor/contractor/settings.py
+++ contractor/contractor/settings.py
@@ -139,6 +139,6 @@ STATICFILES_DIRS =[
     os.path.join(BASE_DIR, "static")
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 WIKI_PAGE_TITLE_MAX_LENGTH = 600
