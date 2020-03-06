baaa26fb4d48b37399a07f4c4c0cc055696b8de7
Patrick <w.patrick.kelly@gmail.com>
2020-03-05 17:46:27 -0800

    fixing edits for event and menu

--- contractor/event_planner/urls.py
+++ contractor/event_planner/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
-from event_planner.views import EventListView, EventDetailView, New_event_form, EventDeleteView, SearchResultsView
+from event_planner.views import EventListView, EventDetailView, New_event_form, EventEditView, EventDeleteView, SearchResultsView
 from menu.views import MenuListView, MenuDetailView, MenuCreateView, MenuEditView, MenuDeleteView
 from . import views
 
@@ -12,9 +12,9 @@ urlpatterns = [
     path('', EventListView.as_view(), name='event-list'),
     path('form/', New_event_form.as_view(), name='new'),
     path('<int:id>/', EventDetailView.as_view(), name='event-details-page'),
-    path('<int:id>/edit/', views.EventEditView.as_view(), name='event-edit-page'),
+    path('<str:slug>/edit/', views.EventEditView.as_view(), name='event-edit-page'),
     path('<int:id>/delete/', views.EventDeleteView.as_view(), name='event-delete-page'),
-    path('search/', SearchResultsView.as_view(), name='search_results'),
+    # path('search/', SearchResultsView.as_view(), name='search_results'),
     path('menu/', MenuListView.as_view(), name='menu-list'),
     path('new-menu/', MenuCreateView.as_view(), name='new-menu-item'),
     path('<int:id>/menu-details/', MenuDetailView.as_view(), name='menu-detail-page'),
